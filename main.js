4ac3ee2a9228be2a2ba30792f091798a1d50fdfb
Cason(Chenxu) Huang <148577178+agedhuang@users.noreply.github.com>
2026-04-01 08:39:41 -0400

    Apply filter and give up about the vite

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ getContentHTML = (content) => {
   let contentHtml = '';
   if (content.type === 'img') {
     contentHtml =
-      `<div class="image-wrapper"> 
-        <img src="${content.content_cn}" alt="${content.annotation_cn || 'Family Huang img'}"> 
+      `<div class="image-wrapper" data-gen="${content.gen_id}"> 
+        <img src="${content.file_path}" alt="${content.annotation_cn || 'Family Huang img'}"> 
       </div>`
   }
 
@@ -27,9 +27,26 @@ let layoutBlocks = (contentData) => {
   let allBlocksHTML = htmlArray.join(''); // Generate HTML for all blocks and join them into a single string.
   content.innerHTML = allBlocksHTML;
   // Re-bind interactions because elements are new!
-  initInteractions();
 
 }  
+
+// setting the filter function ------------------
+const applyFilter = () => {
+  const checkboxes = document.querySelectorAll('.gen-filter:checked');
+  let selectedGens = Array.from(checkboxes).map((cb) => cb.value);
+  
+  const imageWrappers = document.querySelectorAll('.image-wrapper');
+  
+  imageWrappers.forEach((wrapper) => {
+    let wrapperGen = wrapper.dataset.gen;
+    
+    if (selectedGens.includes('all') || selectedGens.includes(wrapperGen)) {
+      wrapper.classList.remove('hidden');
+    } else {
+      wrapper.classList.add('hidden');
+    }
+  });
+};
 
 
 
@@ -45,4 +62,7 @@ fetchJson(`https://opensheet.elk.sh/${googleSheetID}/${tabName2}`, (json) => {
 	console.log("Content data:", json) // See what we get back.
   currentBlocksData = json; // Store the blocks data in the global variable for later use (e.g., shuffle button).
 	layoutBlocks(json) // since I need to group the blocks by each page, 5 blocks for each page, I can't do the foreach() loop here.
+
+  applyFilter(); //firstly apply filter to show all
+  document.querySelector('#filters').addEventListener('change', applyFilter); // Bind the filter function to the change event of the checkboxes.
 })
